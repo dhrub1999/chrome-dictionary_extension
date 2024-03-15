be4a2c88a680f0220fe4eb4b4c4d3837fc2d8c75
Tamal Biswas <tamalbiswas32@gmail.com>
2024-03-15 19:34:53 +0530

    set up done

--- foreground.js
+++ foreground.js
@@ -3,15 +3,4 @@
 // (see "content_script" key).
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
-const axios = require("axios");
-const options = {
-  method: "GET",
-  url: "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
-  headers: {
-    "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
-    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-  },
-};
 
-console.log(process.env.RAPIDAPI_KEY);
-console.log("Hello world");
