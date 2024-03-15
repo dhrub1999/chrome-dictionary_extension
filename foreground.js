38691424431c4743845187a66d40331500610a9b
Tamal Biswas <tamalbiswas32@gmail.com>
2024-03-15 19:33:25 +0530

    updated css styles for the markup

--- foreground.js
+++ foreground.js
@@ -3,5 +3,15 @@
 // (see "content_script" key).
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
+const axios = require("axios");
+const options = {
+  method: "GET",
+  url: "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
+  headers: {
+    "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+  },
+};
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+console.log(process.env.RAPIDAPI_KEY);
+console.log("Hello world");
