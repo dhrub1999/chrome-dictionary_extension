07f2d16bbd9b1e94d50026f49540c6cdbf7b42db
Tamal Biswas <tamalbiswas32@gmail.com>
2024-03-22 20:55:03 +0530

    added api and getting results

--- foreground.js
+++ foreground.js
@@ -1,6 +1,38 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
+const wordMeaning = document.querySelector(".word-meaning");
+const definitionText = document.querySelector(".definition-text");
+const synonymsText = document.querySelector(".synonyms-text");
+const partsOfSpeech = document.querySelector(".parts-of-speech");
+const searchedWord = document.querySelector(".searched-word");
 
+document.getElementById("search-form").addEventListener("submit", (e) => {
+  const outputMsg = document.getElementById("msg");
+  e.preventDefault();
+  const inputVal = document.getElementById("search").value.trim();
+
+  if (inputVal === "") {
+    outputMsg.textContent = "Please enter a word";
+    outputMsg.style.display = "block";
+    outputMsg.style.color = "red";
+    outputMsg.style.transform = "translateY(0)";
+  } else if (/<[^>]*script[^>]*>/i.test(inputVal)) {
+    outputMsg.textContent = "Please enter a valid word";
+  } else {
+    outputMsg.style.display = "none";
+    fetchWordData(inputVal);
+  }
+});
+
+const fetchWordData = async (word) => {
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data[0].meanings[0]);
+    searchedWord.textContent = word;
+
+    wordMeaning.innerHTML = data[0].meanings[0].definitions[0].definition;
+  } catch (error) {
+    console.log(error);
+  }
+};
